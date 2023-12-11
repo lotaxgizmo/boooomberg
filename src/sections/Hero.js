7f815f49cd702d4f0af81840f63e012c29d0e932
Lotachi <lotaoko2@gmail.com>
2023-12-11 12:33:17 +0100

    holla

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -8,35 +8,36 @@ import rocketbg from '../assets/rocketbg.gif'
 function Hero() {
   return (
     <div className='flex flex-col justify-center items-center relative '>
-        
-<div className="text mt-40 z-10">
-<h1 className='text-center text-7xl lg:text-[150px]'>BOOOOMBERG</h1>
-<h1 className='text-center text-9xl mt-[-50px] lg:text-[300px] lg:mt-[-107px] lg:mb-[-57px] mb-[-20px]'>COIN</h1>
-<h2 className='text-center text-2xl lg:text-3xl'>The Wildest Ride in Finance </h2>
-<h3 className='text-center text-2xl lg:text-3xl'>Wall Street's Wildest Nightmare! </h3>
-</div>
+
+      <div className="text mt-40 z-10">
+        <h1 className='text-center text-7xl lg:text-[150px]'>yooooooooooo</h1>
+        <h1 className='text-center text-7xl lg:text-[150px]'>BOOOOMBERG</h1>
+        <h1 className='text-center text-9xl mt-[-50px] lg:text-[300px] lg:mt-[-107px] lg:mb-[-57px] mb-[-20px]'>COIN</h1>
+        <h2 className='text-center text-2xl lg:text-3xl'>The Wildest Ride in Finance </h2>
+        <h3 className='text-center text-2xl lg:text-3xl'>Wall Street's Wildest Nightmare! </h3>
+      </div>
 
 
-<div className="btns z-10">
-<div className="btn flex flex-col lg:flex-row  justify-center items-center pt-10 ">
+      <div className="btns z-10">
+        <div className="btn flex flex-col lg:flex-row  justify-center items-center pt-10 ">
 
-<Btn text={'BOOOOM NOW'}/>
-<Btn2 text={'DEXTOOLS'}/>
-<Btn text={'TELEGRAM'}/>
+          <Btn text={'BOOOOM NOW'} />
+          <Btn2 text={'DEXTOOLS'} />
+          <Btn text={'TELEGRAM'} />
 
-</div>
+        </div>
 
-</div>
+      </div>
 
-<div className="gif flex lg:flex-row flex-col z-10">
-    
-<img src={Capcha} className='' alt="" />
-<img src={Capcha} className='' alt="" />
-<img src={Capcha} className='' alt="" />
+      <div className="gif flex lg:flex-row flex-col z-10">
 
-</div>
-<img src={bggif} className='absolute' alt="" />
-<img src={rocketbg} className='absolute w-[3000px] top-0' alt="" />
+        <img src={Capcha} className='' alt="" />
+        <img src={Capcha} className='' alt="" />
+        <img src={Capcha} className='' alt="" />
+
+      </div>
+      <img src={bggif} className='absolute' alt="" />
+      <img src={rocketbg} className='absolute w-[3000px] top-0' alt="" />
 
     </div>
   )
